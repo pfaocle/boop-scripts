2db508cf3674e0d391c71e8572d1329173eb724a
Paul Byrne <code@pfaocle.co.uk>
2024-12-12 20:18:48 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Custom Boop scripts
+---
+
+Custom scripts for [Boop].
+
+[Boop]: https://github.com/IvanMathy/Boop
---
+++ remove-blank-lines.js
@@ -0,0 +1,20 @@
+/**
+  {
+    "api":1,
+    "name":"Remove blank lines",
+    "description":"Removes all blank lines from your text",
+    "author":"Paul Byrne",
+    "icon":"quote",
+    "tags":"remove,blank,lines"
+  }
+**/
+
+const removeEmptyLines = str => str.split(/\r?\n/).filter(line => line.trim() !== '').join('\n');
+
+function main(state) {
+    let allText = state.fullText;
+    let numBlankLines = allText ? (allText.match(/^[ \t]*$/gm) || []).length : 0;
+
+    state.fullText = removeEmptyLines(allText);
+    state.postInfo(`${ numBlankLines } blank lines removed`);
+}
